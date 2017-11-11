3e38a6bc252b09af7a51b431b5fbce5fa3002b0a
CassioKF <cassiobsk8@hotmail.com>
2017-11-11 12:30:11 -0200

    uPython V2

--- NodeMCU_code/data/python/test.py
+++ NodeMCU_code/data/python/test.py
@@ -1,18 +1,17 @@
 from time import sleep
 import machine
-import os
-import json
+import ujson
 
 led = machine.Pin(13, machine.Pin.OUT)
 spatio = machine.Pin(12, machine.Pin.IN)
 
-def a():
-    se = 'foi'
-    return se
+#def a():
+#    se = 'foi'
+#    return se
 
-def b ():
-    se = a()
-    print(se)
+#def b ():
+#    se = a()
+#    print(se)
 
 
 def spa():
@@ -26,7 +25,7 @@ def spa():
 
 def ler():
     arquivo_json = open('data.json', 'r')
-    dados_json = json.load(arquivo_json)
+    dados_json = ujson.load(arquivo_json)
     arquivo_json.close()
     return dados_json
 
@@ -37,7 +36,7 @@ def pasta():
 def esc():
     dados_json = {'patio': 'DESLIGADA', 'cozinha': 'DESLIGADA', 'sala': 'DESLIGADA', 'escada': 'OFF', 'escritorio': 'OFF', 'quarto': 'OFF', 'patiol': 'OFF', 'cozinhal': 'OFF', 'salal': 'OFF', 'escadal': 'OFF', 'escritoriol': 'OFF', 'quartol': 'OFF', 'gtemp': '0', 'glum': '0', 'gpres': '0'}
     dados_json['patio'] = "TESTE"
-    final_json = json.dumps(dados_json)
+    final_json = ujson.dumps(dados_json)
     arquivo_json = open('data.json', 'w')
     arquivo_json.write(final_json)
     arquivo_json.close()
